8327abf901b97e143fab067ed07242361d7baed5
talbmax10 <91095347+talbmax10@users.noreply.github.com>
2026-08-31 00:27:45 +0300

    Register Room Date converters

--- app/src/main/java/com/example/linuxtermuxpanel/data/local/CommandDatabase.kt
+++ app/src/main/java/com/example/linuxtermuxpanel/data/local/CommandDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.linuxtermuxpanel.data.model.Command
 import com.example.linuxtermuxpanel.data.model.ExecutionHistory
 import com.example.linuxtermuxpanel.data.model.Service
@@ -13,6 +14,7 @@ import com.example.linuxtermuxpanel.data.model.Service
     version = 1,
     exportSchema = false
 )
+@TypeConverters(DatabaseConverters::class)
 abstract class CommandDatabase : RoomDatabase() {
     abstract fun commandDao(): CommandDao
     abstract fun serviceDao(): ServiceDao
